6d7541d5059cea8886f881392d694e9447bd0b5b
Sumanshu Arora <45642655+sumanshusamarora@users.noreply.github.com>
2021-01-08 19:02:15 +1100

    Update and rename infer.py to evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,183 @@
+import os
+import torch
+import ast
+from train_cnn_rnn_crf import (
+    load_data,
+    get_POS_tags,
+    trim_list_of_lists_upto_max_len,
+    tokenize_pos_tags,
+    pad_and_stack_list_of_list,
+    enrich_data,
+)
+import dill
+from torchnlp.encoders.text import pad_tensor
+from torchnlp.datasets.dataset import Dataset
+from torch.utils.data import DataLoader
+import mlflow.pytorch
+
+EXPERIMENT_ID = 0
+RUN_ID = "d40b2cb39125410b8a9b2d0588b142e7"
+
+experiment = mlflow.get_experiment(str(EXPERIMENT_ID))
+artifacts_uri = experiment.artifact_location
+artifcats_location = f"{artifacts_uri}/{RUN_ID}/artifacts/files"
+model_location = f"{artifacts_uri}/{RUN_ID}/artifacts/model"
+params_location =f"{artifacts_uri.replace('file:///','')}/{RUN_ID}/params"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+with open(os.path.join(params_location, 'MAX_SENTENCE_LEN'), 'r') as infile:
+    max_sentence_len = ast.literal_eval(infile.read())
+
+with open(os.path.join(params_location, 'MAX_WORD_LENGTH'), 'r') as infile:
+    max_word_length = ast.literal_eval(infile.read())
+
+with open(os.path.join(params_location, 'TEST_INDEX'), 'r') as infile:
+    TEST_INDEX = ast.literal_eval(infile.read())
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = mlflow.pytorch.load_model(model_location).to(device)
+
+# X Encoder load
+with open(f"mlruns/{EXPERIMENT_ID}/{RUN_ID}/artifacts/files/x_encoder", "rb") as infile:
+    x_encoder = dill.load(infile)
+
+with open(f"mlruns/{EXPERIMENT_ID}/{RUN_ID}/artifacts/files/x_char_encoder", "rb") as infile:
+    x_char_encoder = dill.load(infile)
+
+with open(f"mlruns/{EXPERIMENT_ID}/{RUN_ID}/artifacts/files/y_ner_encoder", "rb") as infile:
+    y_ner_encoder = dill.load(infile)
+
+with open(f"mlruns/{EXPERIMENT_ID}/{RUN_ID}/artifacts/files/tag_to_index", "rb") as infile:
+    tag_to_index = dill.load(infile)
+
+
+X_text_list_as_is, X_text_list, y_ner_list = load_data()
+X_tags, tag_to_index_eval = get_POS_tags(X_text_list)
+X_text_list = trim_list_of_lists_upto_max_len(X_text_list, max_sentence_len)
+X_text_list_as_is = trim_list_of_lists_upto_max_len(X_text_list_as_is, max_sentence_len)
+y_ner_list = trim_list_of_lists_upto_max_len(y_ner_list, max_sentence_len)
+X_tags = trim_list_of_lists_upto_max_len(X_tags, max_sentence_len)
+
+
+alnum, numeric, alpha, digit, lower, title, ascii = enrich_data(X_text_list_as_is)
+
+alnum = pad_and_stack_list_of_list(
+    alnum,
+    max_sentence_len=max_sentence_len,
+    pad_value=-1,
+    tensor_type=torch.FloatTensor,
+)
+numeric = pad_and_stack_list_of_list(
+    numeric,
+    max_sentence_len=max_sentence_len,
+    pad_value=-1,
+    tensor_type=torch.FloatTensor,
+)
+alpha = pad_and_stack_list_of_list(
+    alpha,
+    max_sentence_len=max_sentence_len,
+    pad_value=-1,
+    tensor_type=torch.FloatTensor,
+)
+digit = pad_and_stack_list_of_list(
+    digit,
+    max_sentence_len=max_sentence_len,
+    pad_value=-1,
+    tensor_type=torch.FloatTensor,
+)
+lower = pad_and_stack_list_of_list(
+    lower,
+    max_sentence_len=max_sentence_len,
+    pad_value=-1,
+    tensor_type=torch.FloatTensor,
+)
+title = pad_and_stack_list_of_list(
+    title,
+    max_sentence_len=max_sentence_len,
+    pad_value=-1,
+    tensor_type=torch.FloatTensor,
+)
+ascii = pad_and_stack_list_of_list(
+    ascii,
+    max_sentence_len=max_sentence_len,
+    pad_value=-1,
+    tensor_type=torch.FloatTensor,
+)
+
+x_enriched_features = torch.stack(
+    (alnum, numeric, alpha, digit, lower, title, ascii), dim=2
+)
+
+
+x_encoded = [x_encoder.encode(text) for text in X_text_list]
+x_padded = [pad_tensor(tensor, max_sentence_len) for tensor in x_encoded]
+x_padded = torch.LongTensor(torch.stack(x_padded))
+
+x_char_padded = [
+    [pad_tensor(x_char_encoder.encode(char), max_word_length) for char in word]
+    for word in X_text_list_as_is
+]
+x_char_padded = [
+    pad_tensor(torch.stack(lst), max_sentence_len) for lst in x_char_padded
+]
+x_char_padded = torch.stack(x_char_padded).type(torch.LongTensor)
+
+x_postag_padded = tokenize_pos_tags(
+    X_tags, tag_to_index=tag_to_index, max_sen_len=max_sentence_len
+)
+
+y_ner_encoded = [
+    [y_ner_encoder.encode(label) for label in label_list] for label_list in y_ner_list
+]
+y_ner_padded = [pad_tensor(torch.stack(lst), max_sentence_len) for lst in y_ner_encoded]
+y_ner_padded = torch.stack(y_ner_padded)
+
+x_padded = x_padded[TEST_INDEX]
+x_char_padded = x_char_padded[TEST_INDEX]
+x_postag_padded = x_postag_padded[TEST_INDEX]
+y_ner_padded = y_ner_padded[TEST_INDEX]
+x_enriched_features = x_enriched_features[TEST_INDEX]
+
+
+dataset_infer = Dataset(
+    [
+        {
+            "x_padded": x_padded[i],
+            "x_char_padded": x_char_padded[i],
+            "x_postag_padded": x_postag_padded[i],
+            "y_ner_padded": y_ner_padded[i],
+            "x_enriched_features": x_enriched_features[i],
+        }
+        for i in range(x_padded.shape[0])
+    ]
+)
+
+
+dataloader_infer = DataLoader(dataset=dataset_infer, batch_size=2, shuffle=True)
+
+for i, data_infer in enumerate(dataloader_infer):
+    with torch.no_grad():
+        mask = torch.where(
+            data_infer["x_padded"] > 0,
+            torch.Tensor([1]).type(torch.uint8),
+            torch.Tensor([0]).type(torch.uint8),
+        )
+        out, decoded, crf_loss = model.predict(
+            data_infer["x_padded"].to(device),
+            data_infer["x_postag_padded"].to(device),
+            data_infer["x_char_padded"].to(device),
+            data_infer["x_enriched_features"].to(device),
+            mask.to(device),
+        )
+
+        sentence = [[x_encoder.index_to_token[ind] for ind in x_p] for x_p in data_infer["x_padded"]]
+        true_y = [[y_ner_encoder.index_to_token[word] for word in true] for true in data_infer['y_ner_padded']]
+        result_y = [[y_ner_encoder.index_to_token[word] for word in prediction] for prediction in decoded]
+
+        print(i)
+        out_tuple_list = []
+        for j in range(len(sentence)):
+            out_tuple = [tupe for tupe in tuple(zip(sentence[j], true_y[j], result_y[j])) if tupe[0]!="<pad>" and tupe[1]!="O"]
+            out_tuple_list.append(out_tuple)
+    break
